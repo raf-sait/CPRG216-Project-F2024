715f9eb16c1d4b52fa7b758699451f4a7341c1ca
clayton-sait <clayton.ma@edu.sait.ca>
2024-12-12 23:13:04 -0700

    Update appointment.py

--- appointment.py
+++ appointment.py
@@ -15,6 +15,53 @@ class Appointment:
         self.__day_of_week = day_of_week
         self.__start_time_hour = start_time_hour
 
+
+    def get__client_name(self):
+        return self.__client_name
+    
+    def get__client_phone(self):
+        return self.__client_phone
+    
+    def get_appt_type(self):
+        return self.__appt_type
+    def get_appt_type_desc(self):
+        appt_num = input("Please enter your appointment type from the following options for a description (0,1,2,3,4,): ")
+        if appt_num == 0:
+            print("Available")
+        elif appt_num == 1:
+            print("Mens Cut")
+        elif appt_num == 2:
+            print("Ladies Cut")
+        elif appt_num == 3:
+            print("Mens Colouring")
+        elif appt_num == 4:
+            print("Ladies Colouring")
+        else:
+            print("Not a valid input")
+        
+        return self.get_appt_type_desc
+    
+    def get_end_time_hour(self):
+        return self.__start_time_hour + 1  
+
+    
+    def schedule(self, name, phone, appt):
+        self.__client_name = name
+        self.__client_phone = phone
+        self.__appt_type = appt
+        return 
+    
+    def cancel(self):
+        self.__client_name = 0
+        self.__client_phone = 0
+        self.__appt_type = 0
+        return 
+    
+    def format_record(self):
+        print(f"Appointment details are {self.__client_name}, {self.__client_phone}, {self.__appt_type}, {self.__day_of_week}, {self.__start_time_hour}")
+
+
+
     #def get client_name
     #def get client_phone
     #def get appt_type
